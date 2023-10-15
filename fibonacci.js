92911fb098c6d4e14be2ca1f9a0fc46435b4665b
Akshaya HP <akshayhulkuli@gmail.com>
2023-10-15 16:32:05 +0530

    saved changes

---
+++ fibonacci.js
@@ -0,0 +1,118 @@
+/*
+
+conside below code 
+
+const lib(n) => {
+    if(n == 1) return;
+    lib(n-1)
+    lib(n-1)
+}
+
+                                    lib(6)
+                                    /    \
+                                lib(5)   lib(5)
+                                /    \        /    \
+                            lib(4)   lib(4)   lib(4) lib(4)
+                            /   \     /    \     /  \      /   \
+                        lib(3)  lib(3) lib(3) lib(3) lib(3) lib(3) lib(3) lib(3) lib(3)
+                        /   \     /    \     /  \      /   \ /   \     /    \     /  \      /   \
+                   lib(2)  lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2)  lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2)
+                /   \     /    \     /  \      /   \ /   \     /    \     /  \      /   \ /   \     /    \     /  \      /   \ /   \     /    \     /  \      /   \ /   \     /    \     /  \      /   \ /   \     /    \     /  \      /   \
+lib(2)  lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2)  lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2)  lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2)  lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2) lib(2)     
+
+here no of function call is doubling in each step. (getting multiplied by 2)
+Timecomplexity = 1 + 2 + 4 + 8+ 16 + 32 = 1 + 2^1 + 2^2 +...+ 2^n-1 ~= 2^n ~= 2 ^ (dept of the tree)
+*/
+
+/*
+consider this 
+
+
+const dib(n) => {
+    if(n <= 2) return;
+    dib(n-2)
+    dib(n-2)
+}
+
+                            dib(6)
+                            /    \
+                        dib(4)       dib(4)
+                        /    \        /    \
+                    dib(2)   dib(2) dib(2) dib(2)
+
+here the time complexity is 2^(n/2) ~= 2^n
+*/
+
+// let fib = (n) => {
+//     if(n <= 2) return 1;
+//     return fib(n-1) + fib(n-2);
+// }
+
+
+// console.log(fib(6));
+// console.log(fib(7));
+// console.log(fib(8));
+
+
+/*
+
+
+
+Consider tree for fibonacci problem
+
+                               fib(6)   
+                           /             \       
+                      /                       \
+                 fib(5)                       fib(4)
+                /     \                      /     \
+            fib(4)     fib(3)              fib(3)   fib(2)
+            /    \         /    \           /   \
+        fib(3)   fib(2)   fib(2) fib(1)   fib(2) fib(1)
+        /    \     
+    fib(2)   fib(1) 
+    
+    This tree is equivalent to above tree but the tree stops growing when it hits fib(2) or fib(1) as they are ending conditions.
+    The complexity of this greater than dib() function but less than lib() function.
+    Hence the time complexity is same
+*/
+
+// Time complexity is 2^n and space complexity is n.
+
+// console.log(fib(50));   // takes 2^50 computation.
+
+
+// In order to imporve this solution, we need to use memoization.
+// The best way is to use a Hashmap so that we can store memoized values(previous computed useful values).
+// In javascript let us use object. key will be fibonacci index i and value will be fibonacci number at index i.
+
+const fibOptimized = ((n, memo={}) => {
+    if(n in memo) return memo[n];
+    if(n <= 2) {
+        memo[n] = 1;
+        return memo[n];
+    };
+    memo[n] = fibOptimized(n-1, memo) + fibOptimized(n-2, memo);
+    return memo[n];
+})
+
+console.log(fibOptimized(50));
+
+
+/*
+Now the recursion calls are reduced like this
+
+                        fib(6)
+                        /    \
+                    fib(5)   fib(4)
+                    /    \
+                fib(4)   fib(3)
+                /   \
+            fib(3)  fib(2)
+            /    \ 
+        fib(2)   fib(1)
+
+
+    Time complexity = 1 + 2 + 2 + 2 + 2 ~= n.2 ~= n
+    space complexity = n.(max n entries in the function stack)
+*/
+
---
+++ gridTraveller.js
@@ -0,0 +1,84 @@
+/*
+You will be given with a 2-d grid of size n*m. 
+Find the number ways in which bot can move from the top left corner to the bottom right corner. 
+bot is allowed to make either right or down move.
+*/
+
+/*
+base cases -> 
+1. if you are in a 1*1 grid. you are already in the solution return 1. gT(1,1) => 1.
+2. if you are in any invalid grid cases like gT(0,1) or gT(1,0) or gT(0,0) return 0.
+
+If we move downward we reduce the no of rows by 1.
+If we move right we reduce the no of columns by 1.
+This is how the grid gets simplified per each move.
+
+let us try writting a tree for this. 
+Each node will have 2 branches representing down and right move.
+
+                                         gt(2,3)
+                                     /             \
+                                /                       \
+                        (D) /                                \ (R)
+                        gT(1,3)                              gt(2,2)
+                  (D) /       \ (R)                 (D) /            \ (R)
+                gt(0,3)        gt(1,2)              gt(1,2)          gt(2,1)
+                          (D) /    \ (R)    (D) /      \ (R)  (D) /      \ (R)
+                          gt(0,2)  gt(1,1)  get(0,2)  gt(1,1) gt(1,1)   gt(2,0)
+                                    (ans)              (ans)   (ans)
+           
+    Whereever we get gt(1,1) that represents the solution. 
+    If we hit zero(column or row value) in any case that is the invalid case. candidate for termination.
+    for invalid case return 0. For gt(1,1) return 1. at each parent node return sum of each child.
+*/
+
+
+// const gridTraveller = (m,n) => {
+//     if(m == 1 && n == 1) return 1;
+//     if(m == 0 || n == 0) return 0;
+//     return gridTraveller(m-1, n) + gridTraveller(m, n-1);
+// }
+
+// console.log(gridTraveller(1,1));
+// console.log(gridTraveller(2,3));
+// console.log(gridTraveller(3,2));
+// console.log(gridTraveller(3,3));
+// console.log(gridTraveller(18, 18));
+
+/*
+ans -> 
+1
+3
+3
+6
+2333606220
+*/
+
+// Time complexity is 2^(m+n) and the space complexity is m+n
+
+
+// Optimizing the code by memoization
+// please note gt(a,b) is same as gt(b,a)
+
+const gridTraveller = (m,n, memo = {}) => {
+    // key generation is done based on rule gt(a,b) is same as gt(b,a)
+    // otherwise we don't reduce the time complexity that much.
+    let key = m > n ? m + "," + n : n + "," + m;
+    if((key) in memo) return memo[key];
+    if(m == 1 && n == 1) {
+        memo[key] = 1;
+        return memo[key];
+    } 
+    if(m == 0 || n == 0) {
+        memo[key] = 0;
+        return memo[key];
+    }
+    memo[key] = gridTraveller(m-1, n, memo) + gridTraveller(m, n-1, memo);
+    return memo[key];
+}
+
+console.log(gridTraveller(1,1));
+console.log(gridTraveller(2,3));
+console.log(gridTraveller(3,2));
+console.log(gridTraveller(3,3));
+console.log(gridTraveller(18, 18));
